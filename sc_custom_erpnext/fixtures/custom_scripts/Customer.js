873da94f0223715219e075a03ee4e9ec875d9af5
Ubuntu <ubuntu@ip-172-31-0-172.us-west-2.compute.internal>
2018-02-27 13:17:42 +0000

    New Report Open Items per Sales Order

--- sc_custom_erpnext/fixtures/custom_scripts/Customer.js
+++ sc_custom_erpnext/fixtures/custom_scripts/Customer.js
@@ -2,4 +2,7 @@ frappe.ui.form.on("Customer", "refresh", function(frm) {
 	frm.add_custom_button(__('Open Items'), function() {
 				frappe.set_route('query-report', 'Customer Open Items', {customer:frm.doc.name});
 	});
+	frm.add_custom_button(__('Return Open Items'), function() {
+				frappe.set_route('query-report', 'Customer Open Items per Sales Order', {customer:frm.doc.name});
+	});
 });
