df53e958fb4e11abdb47081be0bbd18522cf124d
Ubuntu <ubuntu@ip-172-31-7-207.us-west-2.compute.internal>
2020-01-11 21:21:35 +0100

    Message

--- sc_custom_erpnext/sc_custom_erpnext/doctype/consignment_stock_entry_item/consignment_stock_entry_item.py
+++ sc_custom_erpnext/sc_custom_erpnext/doctype/consignment_stock_entry_item/consignment_stock_entry_item.py
@@ -6,5 +6,5 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
-class ConsignmentStockEntryDetail(Document):
+class ConsignmentStockEntryItem(Document):
 	pass
